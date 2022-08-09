b815921905cc2e5ed7f48f537cd6c34b19df5926
Tom Collins <t0mc97@gmail.com>
2022-08-09 20:12:25 +0100

    Update css for latest on home page. view all on the right and ful width for mobile

--- includes/homeLatest.php
+++ includes/homeLatest.php
@@ -6,27 +6,18 @@
 
 <div class="homeLatest">
 	<div class="gridContainerHome">
-		<?php 
-			$i = 1;
-			foreach($latestPosts as $latest) { 
-		?>
-
-				<div class="boxNew box_<?php echo $i; ?>">
-					
-					<a href="<?php echo URL_ROOT; ?>blog/<?php echo $latest['slug']; ?>" class="">
-						<?php if($latest['postImages']) { ?>
-							<img src="<?php echo URL_ROOT; ?>images/<?php echo $latest['postImages'][0]['imageName']; ?>" alt="<?php echo $latest['postTitle']; ?>" style="border-radius:15px;" />
-						<?php } ?>
-						<h2><?php echo $latest['postLocation']; ?></h2>
-						<?php echo $latest['postTitle']; ?> <?php echo date('Y', strtotime($latest['postDate'])); ?>
-						<p><?php echo $latest['postBlurb']; ?>...[read more]</p>		
-					</a>
-				</div>
-		<?php
-				$i++; 
-			} 
-		?>
-		<a href="<?php echo URL_ROOT; ?>trip/<?php echo $latest['tripSlug']; ?>" class="viewAll">view all</a>
+		<div class="boxNew box_<?php echo $i; ?>">
+			
+			<a href="<?php echo URL_ROOT; ?>blog/<?php echo $latestPosts[0]['slug']; ?>" class="">
+				<?php if($latestPosts[0]['postImages']) { ?>
+					<img src="<?php echo URL_ROOT; ?>images/<?php echo $latestPosts[0]['postImages'][0]['imageName']; ?>" alt="<?php echo $latestPosts[0]['postTitle']; ?>" style="border-radius:15px;" />
+				<?php } ?>
+				<h2><?php echo $latestPosts[0]['postLocation']; ?></h2>
+				<?php echo $latestPosts[0]['postTitle']; ?> <?php echo date('Y', strtotime($latestPosts[0]['postDate'])); ?>
+				<p><?php echo $latestPosts[0]['postBlurb']; ?>...[read more]</p>		
+			</a>
+			<a href="<?php echo URL_ROOT; ?>trip/<?php echo $latestPosts[0]['tripSlug']; ?>" class="viewAll">view all</a>
+		</div>
 	</div>
 </div>
 		
@@ -41,25 +32,16 @@
 </div>
 <div class="homeLatest">
 	<div class="gridContainerHome">
-		<?php 
-			$i = 1;
-			foreach($latestArticles as $latest) { 
-		?>
-
-				<div class="boxNew box_<?php echo $i; ?>">
-					<a href="<?php echo URL_ROOT; ?>article/<?php echo $latest['slug']; ?>" class="">
-						<?php if($latest['postImage']) { ?>
-							<img src="<?php echo URL_ROOT; ?>images/article/<?php echo $latest['postImage']; ?>" alt="<?php echo $latest['postTitle']; ?>" style="border-radius:15px;" />
-						<?php } ?>
-						<h2><?php echo $latest['postTitle']; ?></h2>
-						<?php echo date('D dS F Y', strtotime($latest['postDate'])); ?>
-						<p><?php echo $latest['postBlurb']; ?>...[read more]</p>	
-					</a>
-				</div>
-		<?php
-				$i++; 
-			} 
-		?>
-		<a href="<?php echo URL_ROOT; ?>articles" class="viewAll">view all</a>
+		<div class="boxNew box_<?php echo $i; ?>">
+			<a href="<?php echo URL_ROOT; ?>article/<?php echo $latestArticles[0]['slug']; ?>" class="">
+				<?php if($latestArticles[0]['postImage']) { ?>
+					<img src="<?php echo URL_ROOT; ?>images/article/<?php echo $latestArticles[0]['postImage']; ?>" alt="<?php echo $latestArticles[0]['postTitle']; ?>" style="border-radius:15px;" />
+				<?php } ?>
+				<h2><?php echo $latestArticles[0]['postTitle']; ?></h2>
+				<?php echo date('D dS F Y', strtotime($latestArticles[0]['postDate'])); ?>
+				<p><?php echo $latestArticles[0]['postBlurb']; ?>...[read more]</p>	
+			</a>
+			<a href="<?php echo URL_ROOT; ?>articles" class="viewAll">view all</a>
+		</div>
 	</div>
 </div>
