faa5652995ffb600e1d2619c73630cb7fb406e98
Tom Collins <t0mc97@gmail.com>
2022-08-28 08:56:53 +0100

    Added image to Our Trips page for each trip

--- includes/trips.php
+++ includes/trips.php
@@ -1,22 +1,34 @@
 <h1 style="padding-top: 0;">Our Trips</h1>
 
-<div class="gridContainer" style="padding-bottom: 2em;">
+<div class="auto-grid">
+	<div class="gallery-grid" style="margin-bottom: 1em;">
 <?php
 	/* Lists all the trips we have been on */
 	if($trips)
 	{
 		foreach($trips as $trip)
 		{
-?>
-			<a class="boxLink" href="<?php echo URL_ROOT; ?>trip/<?php echo $trip['tripSlug']; ?>">	
+			echo '<div class="grid-box boxes">';
+		?>
+			<a class="boxLink" href="<?php echo URL_ROOT; ?>trip/<?php echo $trip['tripSlug']; ?>">
+		<?php
+			if($trip['tripImage'])
+			{
+		?>
+				<?php echo '<img src="' . URL_ROOT . 'images/blog/' . $trip['tripImage'] . '" />'; ?>
+		<?php
+			}
+		?>				
 				<div class="box">
 					<p style="font-weight: bold;"><?php echo $trip['tripName']; ?></p>
 				</div>
 			</a>
 <?php
+			echo '</div>';
 		}
 	}	
 ?>
+	</div>
 </div>
 <div id="map_canvas" style="width: 100%; height: 350px;"></div>
 <div id="trip" data-trips="true"></div>
